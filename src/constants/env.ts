9cb6cceece30e1632fb43fbf97032492f777e15c
minhquan1313 <minhquan1313@gmail.com>
2024-01-08 10:36:57 +0700

    remove duplicate env

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,7 +1,7 @@
-export const VITE_CHAT_SERVER = import.meta.env.VITE_CHAT_SERVER;
 export const VITE_SERVER = import.meta.env.VITE_SERVER;
+export const VITE_API = VITE_SERVER + "/api/v1";
+export const VITE_CHAT_SERVER = VITE_SERVER + "/chat";
 export const VITE_GOOGLE_MAP_API_KEY = import.meta.env.VITE_GOOGLE_MAP_API_KEY;
-export const VITE_API = import.meta.env.VITE_API;
 export const VITE_APP_NAME = import.meta.env.VITE_APP_NAME;
 export const VITE_PUBLIC_PUSH_NOTI_KEY = import.meta.env
   .VITE_PUBLIC_PUSH_NOTI_KEY;
