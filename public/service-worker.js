2f77e311e7a552a1010b7564c140a48220c1b793
minhquan1313 <minhquan1313@gmail.com>
2024-03-19 13:24:34 +0700

    reformat codes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,5 @@
 /* eslint-disable no-undef */
-console.log(
-  `Hello from service worker of Room rent client ${new Date().toString()}`,
-);
+console.log(`Hello from service worker of Room rent client ${new Date().toString()}`);
 
 // export type NotificationPayload = {
 //   title: string;
@@ -9,45 +7,49 @@ console.log(
 //   link: string;
 // };
 
-self.addEventListener("push", (event) => {
-  console.log(`🚀 ~ This push event:`, event);
-  if (event.data) {
-    // console.log("This push event has data: ", event.data.text());
-    console.log("This push event has data: ", event.data.json());
-  } else {
-    console.log("This push event has no data.");
-  }
+function implementWebPush() {
+  self.addEventListener("push", (event) => {
+    console.log(`🚀 ~ This push event:`, event);
+    if (event.data) {
+      // console.log("This push event has data: ", event.data.text());
+      console.log("This push event has data: ", event.data.json());
+    } else {
+      console.log("This push event has no data.");
+    }
 
-  const data = event.data.json();
+    const data = event.data.json();
 
-  const { title, body, link } = data;
+    const { title, body, link } = data;
 
-  const options = {
-    tag: title,
-    title: title,
-    body,
-    icon: "/vite.svg",
-    data: {
-      link,
-    },
-    // icon: "/qr_momo.jpg",
-  };
+    const options = {
+      tag: title,
+      title: title,
+      body,
+      icon: "/vite.svg",
+      data: {
+        link,
+      },
+      // icon: "/qr_momo.jpg",
+    };
 
-  event.waitUntil(self.registration.showNotification(title, options));
-});
+    event.waitUntil(self.registration.showNotification(title, options));
+  });
 
-self.addEventListener("notificationclick", (event) => {
-  console.log(`🚀 ~ self.addEventListener ~ event:`, event);
+  self.addEventListener("notificationclick", (event) => {
+    console.log(`🚀 ~ self.addEventListener ~ event:`, event);
 
-  const clickedNotification = event.notification;
-  clickedNotification.close();
+    const clickedNotification = event.notification;
+    clickedNotification.close();
 
-  const { link } = clickedNotification.data;
+    const { link } = clickedNotification.data;
 
-  if (link) {
-    clients.openWindow(link);
-  }
-  // Do something as the result of the notification click
+    if (link) {
+      clients.openWindow(link);
+    }
+    // Do something as the result of the notification click
 
-  // const promiseChain = doSomething();
-});
+    // const promiseChain = doSomething();
+  });
+}
+
+implementWebPush();
--- src/services/chatPushNotification.ts
+++ src/services/chatPushNotification.ts
@@ -4,80 +4,51 @@ import logger from "@/utils/logger";
 
 export const chatPushNotification = {
   async subscribe(): Promise<PushSubscription | undefined> {
-    // try {
     if ("serviceWorker" in navigator && "PushManager" in window) {
-      const registration = await new Promise<ServiceWorkerRegistration>(
-        (r2) => {
-          navigator.serviceWorker
-            .register("/service-worker.js")
-            .then((registration) => {
-              let serviceWorker;
-              if (registration.installing) {
-                serviceWorker = registration.installing;
-              } else if (registration.waiting) {
-                serviceWorker = registration.waiting;
-              } else if (registration.active) {
-                serviceWorker = registration.active;
-              }
+      const registration = await new Promise<ServiceWorkerRegistration>((rs) => {
+        navigator.serviceWorker.register("/service-worker.js").then((registration) => {
+          const { installing, waiting, active } = registration;
+          const serviceWorker = installing || waiting || active;
 
-              if (serviceWorker) {
-                logger("sw current state", serviceWorker.state);
-                if (serviceWorker.state == "activated") {
-                  logger(`🚀 ~ .then ~ registration:`, registration);
+          if (serviceWorker) {
+            logger("sw current state", serviceWorker.state);
+            if (serviceWorker.state == "activated") {
+              logger(`🚀 ~ .then ~ registration:`, registration);
 
-                  r2(registration);
-                }
+              return rs(registration);
+            }
 
-                serviceWorker.addEventListener(
-                  "statechange",
-                  function (e: any) {
-                    logger("sw statechange : ", e.target?.state);
-                    if (e.target?.state == "activated") {
-                      // use pushManger for subscribing here.
-                      logger(
-                        "Just now activated. now we can subscribe for push notification",
-                      );
-
-                      r2(registration);
-                    }
-                  },
-                );
+            serviceWorker.addEventListener("statechange", function (e) {
+              const { state } = e.target as ServiceWorker;
+              if (state == "activated") {
+                rs(registration);
               }
             });
-        },
-      );
+          }
+        });
+      });
 
       const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: VITE_PUBLIC_PUSH_NOTI_KEY,
-        // applicationServerKey:
-        //   "BKlP2gwfDDdKdZGrJ0zB07NK84h8LwYhDV0ViCYIiV3WUghp1PPGARc159ixrNlpVVVLOTYRLuLvsQ3JuEerM3I",
       });
-
       return subscription;
     }
   },
 
   checkSubscribe() {
-    return new Promise<PushSubscription | null>((r) => {
+    return new Promise<PushSubscription | null>((rs) => {
       navigator.serviceWorker.getRegistrations().then((registrations) => {
-        // logger(
-        //   `🚀 ~ navigator.serviceWorker.getRegistrations ~ registrations:`,
-        //   registrations,
-        // );
-
-        if (!registrations.length) return r(null);
+        if (!registrations.length) return rs(null);
 
         navigator.serviceWorker.ready.then((reg) => {
-          logger(`ready`);
-
           reg.pushManager.getSubscription().then((sub) => {
             if (!sub) {
               // ask user to register for Push
-              return r(sub);
+              return rs(null);
             } else {
               // You have subscription, update the database
-              return r(sub);
+              return rs(sub);
             }
           });
         });
@@ -89,40 +60,32 @@ export const chatPushNotification = {
     return new Promise<boolean | null>((rs) => {
       navigator.permissions
         .query({ name: "notifications" })
-        .then((r) => {
-          logger(`🚀 ~ .then ~ r:`, r);
-
-          if (r.state == "granted") {
+        .then((permission) => {
+          if (permission.state == "granted") {
+            // notifications allowed, gooooo
             rs(true);
-            // notifications allowed, go wild
-          } else if (r.state == "prompt") {
-            rs(null);
+          } else if (permission.state == "prompt") {
             // we can ask the user
-          } else if (r.state == "denied") {
-            rs(false);
+            rs(null);
+          } else if (permission.state == "denied") {
             // notifications were disabled
+            rs(false);
           }
         })
         .catch((e) => {
-          logger(`🚀 ~ checkPermission ~ e:`, e);
+          logger.error(`🚀 ~ checkPermission ~ e:`, e);
+
           rs(null);
         });
     });
   },
 
   removeServiceWorker() {
-    return new Promise<void>((r) => {
+    return new Promise<boolean[]>((rs) => {
       navigator.serviceWorker
         ?.getRegistrations()
-        .then(async function (serviceWorkers) {
-          for await (const i of serviceWorkers) {
-            // logger(`🚀 ~ registration:`, registration);
-
-            await i.unregister();
-            logger(`🚀 ~ forawait ~ i:`, i);
-          }
-          r();
-        });
+        .then((workers) => Promise.all(workers.map((worker) => worker.unregister())))
+        .then(rs);
     });
   },
 
